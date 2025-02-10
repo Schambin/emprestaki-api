eb540947c7a2c2cf2ae6f421fc46b8a863c322c3
Kauan <raidoka546@gmail.com>
2025-02-10 16:54:52 -0400

    created user Types

---
+++ src/users/types/user.ts
@@ -0,0 +1,11 @@
+export type CreateBookInput = {
+    title: string;
+    author: string;
+    category: string;
+};
+
+export type UpdateBookInput = {
+    title?: string;
+    author?: string;
+    category?: string;
+};
