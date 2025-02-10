9f4b454eeb159ce08074ff9662759d37622c1b86
Kauan <raidoka546@gmail.com>
2025-02-10 17:08:05 -0400

    created User middleware with jwt

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,44 +1,27 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from 'jsonwebtoken';
-import prisma from '../prisma/client';
+import jwt from "jsonwebtoken";
+import prisma from "../prisma/client";
 
-export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-
-    if (!token) {
-        return res.status(401).json({ message: 'Authentication required' })
-    }
-
+export const authenticate = async(req: Request, res: Response, next: NextFunction) => {
     try {
+        const token = req.header('Authorization')?.replace('Bearer', '');
+        if(!token) throw new Error();
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { userId: number };
-        const user = await prisma.user.findUnique({
-            where: { id: decoded.userId },
-            select: {
-                id: true,
-                email: true,
-                password: true,
-                role: true,
-                name: true,
-                createdAt: true,
-                updatedAt: true
-            }
-        });
+        const user = await prisma.user.findUnique({ where: { id: decoded.userId }});
+        if(!user) throw new Error();
 
-        if (!user) {
-            return res.status(401).json({ message: 'User not found' });
-        }
-
-        req.user = user
+        req.user = user;
         next();
     } catch (error) {
-        return res.status(401).json({ message: 'Invalid Token' });
+        res.status(401).json({ error: 'Please Authenticate' });
     }
 };
 
-export const authorize = (roles: ('LEITOR' | 'ADMINISTRADOR')[]) => {
+export const authorize = (roles: string[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        if (!req.user || !roles.includes(req.user.role)) {
-            return res.status(403).json({ message: 'Unauthorized access' });
+        if(!roles.includes(req.user.role)) {
+            return res.status(401).json({ error: 'Unauthorized access'})
         }
         next();
     };
