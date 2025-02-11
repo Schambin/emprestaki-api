c0c28c2c4f115ace3901a337bea7a5d279a5ea97
Kauan <raidoka546@gmail.com>
2025-02-11 16:05:50 -0400

    added error to when a book is not avaliable

--- src/loans/errors/loan.errors.ts
+++ src/loans/errors/loan.errors.ts
@@ -11,3 +11,10 @@ export class UnpaidFinesError extends Error {
         this.name = 'UnpaidFinesError';
     }
 }
+
+export class BookNotAvailableError extends Error {
+    constructor(bookId: number) {
+        super(`Book with ID ${bookId} is not available`);
+        this.name = 'BookNotAvailableError';
+    }
+}
