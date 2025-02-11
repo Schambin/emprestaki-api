e77cdb4350c6f9b6d4e79016e863f8db988abd75
Kauan <raidoka546@gmail.com>
2025-02-11 16:10:59 -0400

    created a method to find unpaid fines

--- src/loans/repositories/loan.repository.ts
+++ src/loans/repositories/loan.repository.ts
@@ -2,11 +2,11 @@ import { Prisma } from "@prisma/client";
 import prisma from "../../prisma/client";
 
 export class LoanRepository {
-    async create(data: Prisma.LoanCreateInput){
+    async create(data: Prisma.LoanCreateInput) {
         return prisma.loan.create({ data });
     }
 
-    async findActiveLoansByUser(userId: number){
+    async findActiveLoansByUser(userId: number) {
         return prisma.loan.findMany({
             where: {
                 userId,
@@ -16,27 +16,37 @@ export class LoanRepository {
         });
     }
 
-    async findById(id: number){
+    async findById(id: number) {
         return prisma.loan.findUnique({
             where: { id },
             include: { book: true, user: true }
         });
     }
 
-    async update(id: number, data: Prisma.LoanUpdateInput){
+    async update(id: number, data: Prisma.LoanUpdateInput) {
         return prisma.loan.update({
             where: { id },
             data,
         });
     }
 
-    async findOverdueLoans(){
+    async findOverdueLoans() {
         return prisma.loan.findMany({
             where: {
                 returnDate: null,
                 dueDate: { lt: new Date() }
             },
             include: { book: true, user: true }
+        });
+    }
+
+    async findUnpaidFines(userId: number) {
+        return prisma.loan.findMany({
+            where: {
+                userId,
+                fineAmount: { gt: 0 },
+                paid: false
+            }
         });
     }
 }
