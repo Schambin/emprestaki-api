f556acee9a639fd7f08242e0be5943a40bf12364
Kauan <raidoka546@gmail.com>
2025-02-12 09:44:53 -0400

    created an validation middleware which compares the req.body with the dto passed

---
+++ src/middleware/validate.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express"
+import { plainToInstance } from 'class-transformer';
+import { validate } from "class-validator";
+import { BadRequestError } from "../errors/http.errors";
+
+export const validateRequest = <T extends Object>(dtoClass: new () => T) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const dto = plainToInstance(dtoClass, req.body);
+        const errors = await validate(dto);
+
+        if (errors.length > 0) {
+            const errorMessages = errors.flatMap(error => Object.values(error.constraints || {}));
+
+            return next(new BadRequestError('Validation failed', {
+                errors: errorMessages,
+                fields: errors.map(error => ({
+                    property: error.property,
+                    constraints: error.constraints
+                }))
+            }));
+        }
+
+        req.body = dto;
+        next();
+    }
+}
