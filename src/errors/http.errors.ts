f5f23858869f9f91f5858c165e17996da3230e33
Kauan <raidoka546@gmail.com>
2025-02-13 10:37:40 -0400

    created an http error handler

--- src/errors/http.errors.ts
+++ src/errors/http.errors.ts
@@ -1,12 +1,63 @@
-export class BadRequestError extends Error {
-    public statusCode: number;
-    public details?: Record<string, unknown>;
-
-    constructor(message: string = 'Bad Request', details?: Record<string, unknown>) {
-        super(message);
-        this.name = 'BadRequestError';
-        this.statusCode = 400;
-        this.details = details;
+export class HttpError extends Error {
+    constructor(
+        public readonly statusCode: number, message: string,
+        public readonly details?: Record<string, unknown>
+    ) {
+        super(message)
+        this.name = this.constructor.name;
         Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export class BadRequestError extends HttpError {
+    constructor(message = "Bad request", details?: Record<string, unknown>) {
+        super(400, message, details);
+    }
+}
+
+export class UnauthorizedError extends HttpError {
+    constructor(message = "Unauthorized") {
+        super(401, message);
+    }
+}
+export class ForbiddenError extends HttpError {
+    constructor(message = "Forbidden") {
+        super(403, message);
+    }
+}
+
+export class NotFoundError extends HttpError {
+    constructor(entity: string) {
+        super(404, `${entity} not found`);
+    }
+}
+
+export class ConflictError extends HttpError {
+    constructor(message = "Conflict") {
+        super(409, message);
+    }
+}
+
+export class InternalServerError extends HttpError {
+    constructor(message = "Internal server error") {
+        super(500, message);
+    }
+}
+
+export class DatabaseError extends HttpError {
+    constructor(message: string) {
+        super(500, `Database Error: ${message}`);
+    }
+}
+
+export class LoanLimitExceededError extends HttpError {
+    constructor(maxLoans = 3) {
+        super(400, `Maximum of ${maxLoans} active loans allowed`, { maxLoans });
+    }
+}
+
+export class UnpaidFinesError extends HttpError {
+    constructor() {
+        super(403, "Cannot borrow books with unpaid fines");
     }
 }
