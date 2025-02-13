8f7bc5dd9b6d103eab19f8181fec6899fa5f8a6f
Kauan <raidoka546@gmail.com>
2025-02-13 10:39:27 -0400

    add better error handling and updated findUser logic

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,17 +1,41 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import prisma from "../prisma/client";
+import { SafeUser } from "../users/types/user";
+import { ForbiddenError, UnauthorizedError } from "../errors/http.errors";
 
-export const authenticate = async(req: Request, res: Response, next: NextFunction) => {
+declare module 'express' {
+    interface Request {
+        user?: SafeUser;
+    }
+}
+
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = req.header('Authorization')?.replace('Bearer', '');
-        if(!token) throw new Error();
+
+        if (!token) {
+            throw new UnauthorizedError('Authentication required');
+        }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { userId: number };
-        const user = await prisma.user.findUnique({ where: { id: decoded.userId }});
-        if(!user) throw new Error();
+        const user = await prisma.user.findUnique({
+            where: {
+                id: decoded.userId
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true,
+            }
+        });
 
-        req.user = user;
+        if (!user) {
+            throw new UnauthorizedError('User Not Found');
+        }
+
+        req.user = user as SafeUser;
         next();
     } catch (error) {
         res.status(401).json({ error: 'Please Authenticate' });
@@ -20,9 +44,13 @@ export const authenticate = async(req: Request, res: Response, next: NextFunctio
 
 export const authorize = (roles: string[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        if(!roles.includes(req.user.role)) {
-            return res.status(401).json({ error: 'Unauthorized access'})
+        try {
+            if (!req.user?.role || !roles.includes(req.user.role)) {
+                throw new ForbiddenError();
+            }
+            next();
+        } catch (error) {
+            next(error);
         }
-        next();
     };
 };
