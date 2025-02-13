2f00e9af4d10fdb04721576872b395d2e673e03c
Kauan <raidoka546@gmail.com>
2025-02-13 10:23:31 -0400

    added userRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { checkDatabaseConnection } from './prisma/connect';
 import { bookRoutes } from './routes/book.routes';
 import { loanRoutes } from './routes/loan.routes';
-import { checkDatabaseConnection } from './prisma/connect';
+import { userRoutes } from './routes/user.routes';
 
 export const app = express()
 
@@ -10,6 +11,7 @@ checkDatabaseConnection();
 app.use(express.json())
 app.use('/api/books', bookRoutes())
 app.use('/api/loans', loanRoutes())
+app.use('/api/user', userRoutes())
 
 const port = process.env.PORT || 8888
 app.listen(port, () => {
