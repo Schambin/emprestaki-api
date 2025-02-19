2e5bdf5a8d7215d77849aa4645aadbb1dec7bcc5
Kauan <raidoka546@gmail.com>
2025-02-19 09:58:01 -0400

    made the start server a function

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,28 @@ import { checkDatabaseConnection } from './prisma/connect';
 import { bookRoutes } from './routes/book.routes';
 import { loanRoutes } from './routes/loan.routes';
 import { userRoutes } from './routes/user.routes';
+import seedAdminUser from './utilities/seed';
 
 export const app = express()
+const port = process.env.PORT || 8888;
 
-checkDatabaseConnection();
+async function startServer() {
+    try {
+        await checkDatabaseConnection();
+        await seedAdminUser();
 
-app.use(express.json())
-app.use('/api/books', bookRoutes())
-app.use('/api/loans', loanRoutes())
-app.use('/api/user', userRoutes())
+        app.use(express.json())
+        app.use('/api/books', bookRoutes())
+        app.use('/api/loans', loanRoutes())
+        app.use('/api/users', userRoutes())
 
-const port = process.env.PORT || 8888
-app.listen(port, () => {
-    console.log(`App is runing on PORT ${port}`)
-})
+        app.listen(port, () => {
+            console.log(`App is runing on PORT ${port}`)
+        })
+    } catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
+}
+
+startServer();
