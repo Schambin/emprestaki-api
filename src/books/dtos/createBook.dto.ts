edf70365e3f237a396e744a6416538cd69cbb7cc
Kauan <raidoka546@gmail.com>
2025-02-19 09:58:59 -0400

    add error message to the class-validator

--- src/books/dtos/createBook.dto.ts
+++ src/books/dtos/createBook.dto.ts
@@ -1,15 +1,17 @@
-import { IsNotEmpty, IsString } from "class-validator"
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateBookDto {
-    @IsString()
-    @IsNotEmpty()
-    title!: string
+    @IsString({ message: 'Title must be a string' })
+    @IsNotEmpty({ message: 'Title is required' })
+    title!: string;
 
-    @IsString()
-    @IsNotEmpty()
-    author!: string
+    @IsString({ message: 'Author must be a string' })
+    @IsNotEmpty({ message: 'Author is required' })
+    author!: string;
 
-    @IsString()
-    @IsNotEmpty()
-    category!: string
+    @IsString({ message: 'Category must be a string' })
+    @IsNotEmpty({ message: 'Category is required' })
+    category!: string;
+
+    status!: string
 }
