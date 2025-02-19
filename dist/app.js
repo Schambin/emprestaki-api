dea5ed9068322ecabc9835eaa60eacd9c5ea5dda
Kauan <raidoka546@gmail.com>
2025-02-19 09:57:43 -0400

    updated build app

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,13 +18,26 @@ const connect_1 = require("./prisma/connect");
 const book_routes_1 = require("./routes/book.routes");
 const loan_routes_1 = require("./routes/loan.routes");
 const user_routes_1 = require("./routes/user.routes");
+const seed_1 = __importDefault(require("./utilities/seed"));
 exports.app = (0, express_1.default)();
-(0, connect_1.checkDatabaseConnection)();
-exports.app.use(express_1.default.json());
-exports.app.use('/api/books', (0, book_routes_1.bookRoutes)());
-exports.app.use('/api/loans', (0, loan_routes_1.loanRoutes)());
-exports.app.use('/api/user', (0, user_routes_1.userRoutes)());
 const port = process.env.PORT || 8888;
-exports.app.listen(port, () => {
-    console.log(`App is runing on PORT ${port}`);
-});
+function startServer() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield (0, connect_1.checkDatabaseConnection)();
+            yield (0, seed_1.default)();
+            exports.app.use(express_1.default.json());
+            exports.app.use('/api/books', (0, book_routes_1.bookRoutes)());
+            exports.app.use('/api/loans', (0, loan_routes_1.loanRoutes)());
+            exports.app.use('/api/users', (0, user_routes_1.userRoutes)());
+            exports.app.listen(port, () => {
+                console.log(`App is runing on PORT ${port}`);
+            });
+        }
+        catch (error) {
+            console.error('Failed to start server:', error);
+            process.exit(1);
+        }
+    });
+}
+startServer();
