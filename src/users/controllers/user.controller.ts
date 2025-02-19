7fe5e851955b87ebf941c3be3ea2441a3bc63689
Kauan <raidoka546@gmail.com>
2025-02-19 17:39:20 -0400

    created method to get user data with loans

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -1,17 +1,32 @@
 import { Request, Response } from 'express';
 import { UserService } from '../services/user.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { SafeUser } from '../../users/types/user';
 
 export class UserController {
     constructor(private userService = new UserService()) { };
-
     async createUser(req: Request) {
+        const user = req.user;
+        req.user = user as SafeUser;
         return this.userService.createUser(req.body as CreateUserDto);
     }
 
     async getUserById(req: Request, res: Response) {
         const user = await this.userService.getUserById(parseInt(req.params.id));
         res.json({ user });
+    }
+
+    async getCurrentUser(req: Request, res: Response) {
+        try {
+            const user = req.user;
+            req.user = user as SafeUser;
+            const userWithLoans = await this.userService.getUserWithLoans(req.user.id);
+            res.json(userWithLoans);
+        } catch (error) {
+            res.status(500).json({
+                error: error instanceof Error ? error.message : 'Failed to fetch profile'
+            });
+        }
     }
 
     async updateUser(req: Request, res: Response) {
