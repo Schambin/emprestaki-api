52631683fa4f3f8b44810f9580cea266b97e0c03
Kauan <raidoka546@gmail.com>
2025-02-21 15:53:44 -0400

    removing unused code

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-import express from 'express';
 import { checkDatabaseConnection } from './prisma/connect';
 import { bookRoutes } from './routes/book.routes';
 import { loanRoutes } from './routes/loan.routes';
 import { userRoutes } from './routes/user.routes';
 import seedAdminUser from './utilities/seed';
-import { errorHandler } from './middleware/error.middleware';
+import express from 'express';
 
 export const app = express()
 const port = process.env.PORT || 8888;
